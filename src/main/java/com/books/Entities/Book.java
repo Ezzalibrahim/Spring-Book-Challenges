5f33777687db2b36725ee8f012f7ab015c3bcaf3
Ezzalibrahim <brahim.ezzali@hotmail.com>
2024-02-16 16:44:58 +0100

    add jpa annotations to mapping to table in db

--- src/main/java/com/books/Entities/Book.java
+++ src/main/java/com/books/Entities/Book.java
@@ -2,6 +2,7 @@ package com.books.Entities;
 
 
 import com.books.Enums.BookCategories;
+import jakarta.persistence.*;
 import lombok.*;
 
 //@Getter @Setter
@@ -9,14 +10,19 @@ import lombok.*;
 @AllArgsConstructor
 @Data
 
-
+@Entity
+@Table(name = "books")
 public class Book {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "book_title" , length = 100 , unique = true )
     private String title;
+
+
     private double price;
     private BookCategories category; // action & Action & ACtion
-
 
 }
