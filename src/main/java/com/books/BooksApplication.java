ec599138f8c3d05e56dafb42b534e6e077fa6eda
Ezzalibrahim <brahim.ezzali@hotmail.com>
2024-03-05 18:00:25 +0100

    comment the unnecessary code

--- src/main/java/com/books/BooksApplication.java
+++ src/main/java/com/books/BooksApplication.java
@@ -11,12 +11,23 @@ public class BooksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooksApplication.class, args);
-		Book book = new Book(12 , "test" ,  12 ,BookCategories.Action);
-		System.out.println(book.getTitle());
-		System.out.println(book.getPrice());
-		book.setPrice(53);
-		book.setTitle("test 1");
-		System.out.println(book);
+//		Book book = new Book(12 , "test" ,  12 ,BookCategories.Action);
+//		System.out.println(book.getTitle());
+//		System.out.println(book.getPrice());
+//		book.setPrice(53);
+//		book.setTitle("test 1");
+//		System.out.println(book);
+
+		// DTO
+		// create DB in mysql
+		// relations between tables
+		// oneToMany => A oneToMany B =>
+		// ManyToMany
+		// DAO
+		// exception handling
+		// Jpa repo & CRUD repo
+		// based paging & sorting in jpa
+		//
 	}
 
 
