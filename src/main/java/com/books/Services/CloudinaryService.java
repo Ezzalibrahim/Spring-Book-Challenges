89d1393d8fd53a9d12ad43b4d98693fe7fb1863b
Ezzalibrahim <brahim.ezzali@hotmail.com>
2024-05-05 23:58:27 +0100

    add the ability to upload multiple files

--- src/main/java/com/books/Services/CloudinaryService.java
+++ src/main/java/com/books/Services/CloudinaryService.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -31,4 +33,14 @@ public class CloudinaryService  {
             return null;
         }
     }
+
+    public List<String> uploadFileList(List<MultipartFile> files, String folderName ){
+        List<String> filesName = new ArrayList<>();
+        for (MultipartFile file : files) {
+            filesName.add(uploadFile(file , folderName));
+        }
+
+        return filesName;
+    }
+
 }
